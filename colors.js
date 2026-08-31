416dff3e11998b1be7dfd72ac2a02d47fa58bc67
ahmedateem23-cell <ahmedateem23@gmail.com>
2026-08-31 17:04:55 +0200

    Create colors.js

---
+++ colors.js
@@ -0,0 +1,13 @@
+export const COLORS = {
+  ivory: '#F5F1E8',
+  ivoryLight: '#FAF8F3',
+  ivoryDark: '#E8DFD0',
+  gold: '#8B7355',
+  goldLight: '#C9B99A',
+  charcoal: '#5C2029',
+  ink: '#2B2622',
+  textMuted: '#6B5B4F',
+  textSubtle: '#A99E8E',
+  border: '#DDD5C4',
+  white: '#FFFFFF',
+};
